8dff286c8f1a0a2117e4055f4a98efd18c92cfa9
Huỳnh Minh Hưng <70432453+minhhunghuynh1106@users.noreply.github.com>
2021-04-28 14:01:47 -0700

    complete drag slider

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-let tagsCtn = document.querySelector('.tags'); // get element of tags container
-let isDown = false;
-let startX;
-let scrollLeft;
+function dragSlider() {
+    let tagsCtn = document.querySelector('.tags'); // get element of tags container
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    
+    tagsCtn.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX - tagsCtn.offsetLeft;
+        scrollLeft = tagsCtn.scrollLeft;
+    })
+    
+    tagsCtn.addEventListener('mouseup', () => {
+        isDown = false;
+    })
+    
+    tagsCtn.addEventListener('mouseleave', () => {
+        isDown = false;
+    })
+    
+    tagsCtn.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+    
+        const x = e.pageX - tagsCtn.offsetLeft;
+        const walk = x - startX;
+        console.log({x, startX, scrollLeft});
+        tagsCtn.scrollLeft =  scrollLeft - walk;
+    })
+}
 
-tagsCtn.addEventListener('mousedown', (e) => {
-    isDown = true;
-    startX = e.pageX - tagsCtn.offsetLeft;
-    scrollLeft = tagsCtn.scrollLeft;
-})
-
-tagsCtn.addEventListener('mouseup', () => {
-    isDown = false;
-})
-
-tagsCtn.addEventListener('mouseleave', () => {
-    isDown = false;
-})
-
-tagsCtn.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-
-    const x = e.pageX - tagsCtn.offsetLeft;
-    const walk = x - startX;
-    console.log({x, startX, scrollLeft});
-    tagsCtn.scrollLeft = scrollLeft - walk;
-})
+dragSlider();
