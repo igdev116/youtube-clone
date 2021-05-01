3722d046fd6ed524375a5a3b31cd9bf3b2d407ca
Huỳnh Minh Hưng <70432453+minhhunghuynh1106@users.noreply.github.com>
2021-04-30 17:03:25 -0700

    fix drag slider

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
+function transValue(value) {
+    return Number(value.replace(/[^-?\d.]/g, ''));
+}
+
 function dragSlider() {
     let tagsCtn = document.querySelector('.tags'); // get element of tags container
-    let isDown = false;
-    let startX;
-    let scrollLeft = 0;
-    let updateValue = 0;
-    
+    let isDown = false,
+        startX,
+        scrollLeft = 0,
+        updateValue = 0;
+
     tagsCtn.addEventListener('mousedown', (e) => {
         isDown = true;
         startX = e.pageX - tagsCtn.offsetLeft;
-        updateValue += scrollLeft;
+        updateValue += scrollLeft; // value will be updated every next click
     })
-    
+
     tagsCtn.addEventListener('mouseup', () => {
         isDown = false;
         console.log('Done');
     })
-    
+
     tagsCtn.addEventListener('mouseleave', () => {
         isDown = false;
     })
-    
+
     tagsCtn.addEventListener('mousemove', (e) => {
         if (!isDown) return;
         e.preventDefault();
-
         const x = e.pageX - tagsCtn.offsetLeft;
-        const walk = x - startX;
+        let walk = x - startX;
         scrollLeft = 0 + walk;
-
+       
         tagsCtn.style.transform = `translateX(${updateValue + walk}px)`;
+
+        if (transValue(tagsCtn.style.transform) > 0) {
+            tagsCtn.style.transform = `translateX(0px)`;
+            updateValue = 0;
+        }
+
         // clickSlider(walk);
     })
 }
+
 
 dragSlider();
 
